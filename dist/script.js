d378e511ae9b407063c30fe73b59b04e43d6ce4e
Ashish Pondit <ashishpondit@gmail.com>
2024-03-14 23:12:39 +0600

    Fix flip countdown

--- dist/script.js
+++ dist/script.js
@@ -2,47 +2,71 @@ const focusTime = 25;
 const restTime = 5;
 
 let focus = true;
-let currentTime = focusTime*60+1;
+let currentTime = focusTime * 60 + 1;
 
-function hTime(timeSeconds){
-  let minute = Math.floor(timeSeconds/60);
-  let second = timeSeconds%60;
+function hTime(timeSeconds) {
+    let minute = Math.floor(timeSeconds / 60);
+    let second = timeSeconds % 60;
 
-  let finalTime = '';
-  if(minute <10){
-    finalTime += '0'+minute.toString();
-  } else{
-    finalTime += minute.toString();
-  }
+    let finalTime = "";
+    if (minute < 10) {
+        finalTime += "0" + minute.toString();
+    } else {
+        finalTime += minute.toString();
+    }
 
-  // finalTime += ':';
+    // finalTime += ':';
 
-  if(second <10){
-    finalTime += '0'+second.toString();
-  } else{
-    finalTime += second.toString();
-  }
-  return finalTime;
+    if (second < 10) {
+        finalTime += "0" + second.toString();
+    } else {
+        finalTime += second.toString();
+    }
+    return finalTime;
 }
 
-function setupFlip(tick) {
-
- Tick.helper.interval(function() {
-  if(currentTime == 0){
-    focus = !focus;
-    if(focus){
-      currentTime = focusTime*60+1;
+function getMinute(timeSeconds) {
+    let minute = Math.floor(timeSeconds / 60);
+    if(minute < 10) {
+      return '0'+minute.toString();
     }else{
-      currentTime = restTime*60+1;
+      return minute.toString();
     }
-  }
+}
 
-  currentTime--;
-  let time = hTime(currentTime);
-  tick.value = time;
+function getSecond(timeSeconds) {
+    let second = timeSeconds % 60;
+    if(second<10){
+      return '0'+second.toString();
+    }else{
+      return second.toString();
+    }
+}
 
-  // const node = document.querySelector('.tick-flip:nth-child(3)')
-  
- }, 1000);
+function setTime() {
+    if (currentTime == 0) {
+        focus = !focus;
+        if (focus) {
+            currentTime = focusTime * 60 + 1;
+        } else {
+            currentTime = restTime * 60 + 1;
+        }
+    }
 
+    currentTime--;
+}
+
+function setupMinute(tick) {
+    Tick.helper.interval(function () {
+        let time = getMinute(currentTime);
+        tick.value = time;
+    }, 1000);
+}
+
+function setupSecond(tick) {
+  Tick.helper.interval(function () {
+    setTime();
+      let time = getSecond(currentTime);
+      tick.value = time;
+  }, 1000);
 }
