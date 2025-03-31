84c82aa5799f012437b4604131816b16ee3070d4
Ashish Pondit <ashishpondit@gmail.com>
2025-03-31 13:31:05 +0600

    Add count down timer

--- background.js
+++ background.js
@@ -1,10 +1,8 @@
 chrome.runtime.onInstalled.addListener(() => {
-    console.log("event captured on install");
-    chrome.tabs.create({ url: "dist/index.html" });
+  chrome.tabs.create({ url: "dist/index.html" });
 });
 
 chrome.action.onClicked.addListener((tab) => {
-    // Listen for click on extension icon
-    console.log("click event captured");
-    chrome.tabs.create({ url: "dist/index.html" });
+  // Listen for click on extension icon
+  chrome.tabs.create({ url: "dist/index.html" });
 });
--- dist/script.js
+++ dist/script.js
@@ -1,64 +1,68 @@
-const FOCUS_TIME = 25*60;
-const REST_TIME = 5*60;
+const FOCUS_TIME = 25;
+const REST_TIME = 10;
 
 const audio_controller = document.querySelector(".audio-controll");
 
-let focusTime = FOCUS_TIME;
-let restTime = REST_TIME;
-let state = "focus";
-let pause = false;
-
-let loadingCount = 1;
-let loadingChar = ".";
-let loadingLimit = 3;
+let timer = JSON.parse(localStorage.getItem("timer")) || FOCUS_TIME;
+let state = localStorage.getItem("state") || "focus";
+let pause = JSON.parse(localStorage.getItem("pause")) || false;
+let audioPaused = JSON.parse(localStorage.getItem("audioPaused")) || true;
+let audioSrc = localStorage.getItem("audioSrc") || "";
 
 let bgImageIndex = 0;
 const bgImages = [
-  "https://storage.googleapis.com/pai-images/892e2bfb5df749a3b0f404229d79e864.jpeg",
-  "https://wallpapercave.com/wp/wp4697718.jpg",
-  "https://wallpapercave.com/wp/wp11882328.jpg"
+  "./static/bg-1.jpg",
+  "./static/bg-2.jpg",
+  "./static/bg-3.jpg",
+  "./static/bg-4.jpg",
+  "./static/bg-5.jpg",
 ];
 
 const audioLibrary = {
-  "fire": "https://mynoise.world/NoisesOnline/Audio/ea.ogg",
-  "water": "https://mynoise.world/NoisesOnline/Audio/aa.ogg",
-  "bird": "https://mynoise.world/NoisesOnline/Audio/da.ogg",
+  fire: "https://mynoise.world/NoisesOnline/Audio/ea.ogg",
+  water: "https://mynoise.world/NoisesOnline/Audio/aa.ogg",
+  bird: "https://mynoise.world/NoisesOnline/Audio/da.ogg",
   "ding-dong": "https://mynoise.world/NoisesOnline/Audio/gb.ogg",
-  "meditation": "https://mynoise.world/NoisesOnline/Audio/ga.ogg",
-}
+  meditation: "https://mynoise.world/NoisesOnline/Audio/ga.ogg",
+};
 
-function changeSound(event){
-  const soundType = this.getAttribute('data-sound-type');
+function changeSound(event) {
+  const soundType = this.getAttribute("data-sound-type");
   let target = event.currentTarget;
-  if(target.classList.contains("button-active")){
+  if (target.classList.contains("button-active")) {
     audio_controller.pause();
     audio_controller.src = "";
     target.classList.remove("button-active");
-  }else{
- document.querySelectorAll(".audio-collection span").forEach((item)=>{
+  } else {
+    document.querySelectorAll(".audio-collection span").forEach((item) => {
       item.classList.remove("button-active");
-    })
-    
+    });
+
     audio_controller.pause();
     audio_controller.src = audioLibrary[soundType];
-    if(!pause){
+    if (!pause) {
       audio_controller.play();
-    }else{
+    } else {
       audio_controller.pause();
     }
     target.classList.add("button-active");
   }
 }
 
-function wrapperFunction(event){
-  const soundType = this.getAttribute('data-sound-type');
+function wrapperFunction(event) {
+  const soundType = this.getAttribute("data-sound-type");
   changeSound(event, soundType);
 }
+
 document.querySelector("#sound-water").addEventListener("click", changeSound);
 document.querySelector("#sound-fire").addEventListener("click", changeSound);
 document.querySelector("#sound-bird").addEventListener("click", changeSound);
-document.querySelector("#sound-ding-dong").addEventListener("click", changeSound);
-document.querySelector("#sound-meditation").addEventListener("click", changeSound);
+document
+  .querySelector("#sound-ding-dong")
+  .addEventListener("click", changeSound);
+document
+  .querySelector("#sound-meditation")
+  .addEventListener("click", changeSound);
 
 function dynamicBackground() {
   if (bgImages.length > 0) {
@@ -85,16 +89,20 @@ function controlPomodoro() {
   }
 }
 
-document
-  .querySelector(".controller")
-  .addEventListener("click", controlPomodoro);
+function setPomodoroControl() {
+  document
+    .querySelector(".controller")
+    .addEventListener("click", controlPomodoro);
 
-function loading() {
-  document.querySelector(".loading").innerText = loadingChar.repeat(
-    loadingCount++
-  );
-  loadingCount = loadingCount > loadingLimit ? 1 : loadingCount;
+  if (!pause) {
+    document.querySelector(".controller .pause").style.display = "block";
+    document.querySelector(".controller .play").style.display = "none";
+  } else {
+    document.querySelector(".controller .pause").style.display = "none";
+    document.querySelector(".controller .play").style.display = "block";
+  }
 }
+setPomodoroControl();
 
 function setTimeText(timeString) {
   document.querySelector("time").innerText = timeString;
@@ -115,38 +123,54 @@ function secondsToText(totalSeconds) {
   setTimeText(timeString);
 }
 
-function focusTimer() {
-  if (focusTime === 0) {
-    state = "rest";
-    restTime = REST_TIME;
-    document.querySelector(".time-text").innerText = "Resting";
-  } else {
-    focusTime--;
-    secondsToText(focusTime);
+function playCountDown() {
+  audioPaused = audio_controller.paused;
+  audioSrc = audio_controller.src;
+
+  audio_controller.src = "./assets/countdown.wav";
+  if (!audioPaused) {
+    audio_controller.play();
   }
 }
 
-function restTimer() {
-  if (restTime === 0) {
-    state = "focus";
-    focusTime = FOCUS_TIME;
-    document.querySelector(".time-text").innerText = "Focusing";
-    dynamicBackground();
-  } else {
-    restTime--;
-    secondsToText(restTime);
+function resumeAudio() {
+  audio_controller.src = audioSrc;
+  if (!audioPaused) {
+    audio_controller.play();
   }
 }
 
 function timerFunction() {
   if (!pause) {
-    if (state == "rest") {
-      restTimer();
-    } else {
-      focusTimer();
+    if (timer === 0 && state === "focus") {
+      state = "rest";
+      timer = REST_TIME;
+      document.querySelector(".time-text").innerText = "Resting";
+      resumeAudio();
+    } else if (timer === 0 && state === "rest") {
+      state = "focus";
+      timer = FOCUS_TIME;
+      document.querySelector(".time-text").innerText = "Focusing";
+      dynamicBackground();
+      resumeAudio();
     }
-    loading();
+
+    if (timer === 4) {
+      playCountDown();
+    }
+
+    timer--;
+    secondsToText(timer);
   }
 }
 
+function saveState() {
+  localStorage.setItem("timer", timer);
+  localStorage.setItem("state", state);
+  localStorage.setItem("pause", pause);
+  localStorage.setItem("audioPaused", audioPaused);
+  localStorage.setItem("audioSrc", audioSrc);
+}
+
 setInterval(timerFunction, 1000);
+setInterval(saveState, 5000);
